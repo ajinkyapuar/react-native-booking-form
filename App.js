a9f89fd8ccc6701702542dcbccfe6660661b0523
Ajinkya <albus702@gmail.com>
2017-09-12 13:49:49 +0530

    loaded font async, text font setup

--- App.js
+++ App.js
@@ -1,15 +1,49 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Font } from 'expo';
 
 export default class App extends React.Component {
+
+  constructor(props){
+    super(props);
+  }
+
+  state = {
+    appIsReady: false,
+
+  }
+
+  async componentDidMount() {
+
+    await Font.loadAsync({
+      'open-sans-regular': require('./assets/fonts/OpenSans-Regular.ttf'),
+    });
+
+    this.setState({ appIsReady: true });
+
+
+  }
+
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+    if (this.state.appIsReady) {
+      return (
+        <View style={styles.container}>
+        <Text style={[styles.flexRow, styles.text, styles.headingText]}>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
+        </View>
+      );
+    }
+    else {
+      return (
+        <View style={styles.container}>
+        <Text>
+        Loading...
+        </Text>
+        </View>
+      );
+
+    }
   }
 }
 
@@ -19,5 +53,15 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  flexRow: {
+    flex: 0.1,
+    flexDirection: 'row',
+    // justifyContent: 'center',
+    // alignItems: 'center',
+  },
+  text: {
+    fontFamily: 'open-sans-regular',
+    // fontSize: 16
   },
 });
